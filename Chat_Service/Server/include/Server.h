eccb3e090dfa4d6d4228b253e40f45408ebcc0a7
fxanhkhoa <fxanhkhoa@gmail.com>
2018-06-19 17:55:21 +0700

    add Server.h

---
+++ Chat_Service/Server/include/Server.h
@@ -0,0 +1,24 @@
+#ifndef _SERVER_H
+#define _SERVER_H
+
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <string.h>
+
+class Server
+{
+public:
+  int server_fd;
+  int new_socket;
+  int valread;
+  struct sockaddr_in address;
+  int opt = 1;
+  int addrlen = sizeof(address);
+  char buffer[1024] = {0};
+  char *hello = "Hello from server";
+};
+
+#endif
